e64df3340dfd9b6fd1d3451106751409a905427e
bilge <bilgenurmert@icloud.com>
2023-10-30 22:59:36 +0300

    Spherical coordinate system     - Added necessary parameters of "Spherical coordinate system"

--- src/controller/include/obstacle_avoidance.hpp
+++ src/controller/include/obstacle_avoidance.hpp
@@ -1,6 +1,12 @@
 #ifndef __OBSTACLE_AVOIDANCE_HPP__
 #define __OBSTACLE_AVOIDANCE_HPP__
 
+#define VERTICAL   181 
+#define HORIZONTAL 360
+#define ERROR (-1)
+#define OK    (1)
+
+
 // class ObstacleAvoidance{
 // private:
 
