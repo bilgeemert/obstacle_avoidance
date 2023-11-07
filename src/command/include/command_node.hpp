d9a74e6e3529c0da201051c342a0c07ff0e5dcf7
bilge <bilgenurmert@icloud.com>
2023-11-07 21:35:04 +0300

    command node fixed to receive data from serial

--- src/command/include/command_node.hpp
+++ src/command/include/command_node.hpp
@@ -10,8 +10,25 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/joy.hpp"
+#include "command_type.hpp"
 
 using joyMsg = sensor_msgs::msg::Joy;
 
+class Command: public rclcpp::Node{
+private:
+    rx_t data;
+    joyMsg msg;
+    device_t device;
+    rclcpp::TimerBase::SharedPtr timer_;  
+    rclcpp::Publisher<joyMsg>::SharedPtr joy_pub;
+
+public:
+    Command();
+    ~Command();
+    bool initPort();
+    bool configure();
+    void dataRead();
+    void initParam();
+};
 
 #endif
